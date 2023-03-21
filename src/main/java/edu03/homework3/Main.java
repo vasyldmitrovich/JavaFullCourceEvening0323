f481edf78876b0ad65955374efd05cbf96c969f5
Tanya Serbinenko <tanyaserbinenko1996@gmail.com>
2023-03-21 13:57:53 +0200

    init

---
+++ src/main/java/edu03/homework3/Main.java
@@ -0,0 +1,17 @@
+package edu03.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Tim", "Burton");
+        Person person2 = new Person("Anna", "Karenina");
+        Person person3 = new Person("Taras", "Shevchenko");
+        Person person4 = new Person("Ivan", "Mazepa");
+        Person person5 = new Person("Edgar Alan", "Po");
+        person1.setBirthYear(1958);
+        System.out.println(Methods.getAge(person1));
+        person3.setBirthYear(1814);
+        Methods.output(person3);
+        Methods.changeName(person2, "Tetiana");
+
+    }
+}
---
+++ src/main/java/edu03/homework3/Methods.java
@@ -0,0 +1,18 @@
+package edu03.homework3;
+
+public class Methods {
+    private static final int currentYear = 2023;
+
+    public static int getAge(Person person) {
+        return currentYear - person.getBirthYear();
+    }
+
+    public static void output(Person person) {
+        System.out.println("Name: " + person.getFirstName() + " LastName: " + person.getLastName() + " Year of birth: " + person.getBirthYear());
+    }
+
+    public static void changeName(Person person, String ln) {
+        person.setFirstName(ln);
+        output(person);
+    }
+}
