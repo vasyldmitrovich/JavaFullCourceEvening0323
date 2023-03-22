4c1087368951c54649dbc20fcdd4848ced109b70
Ivan Yashchuk <ivanko3224@gmail.com>
2023-03-22 18:47:24 +0200

    Create Main.java

---
+++ src/com/softserve/edu01/hw/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        rad.radius();
+        adr.adress();
+        ph.phone();
+
+    }
+
+
+}
+ class  adr{
+    public static void adress(){
+        Scanner n = new Scanner(System.in);
+        System.out.println("Enter your name -->  ");
+        String name = n.nextLine();
+        System.out.println("Enter your adress " + name);
+        Scanner a = new Scanner(System.in);
+        String adress = a.nextLine();
+        System.out.println("Hi " + name + " your adress is " + adress);
+        System.out.println();
+    }
+}
+
+class rad{
+    public static  void radius(){
+        float p = 3.14f;
+        Scanner radius = new Scanner(System.in);
+        System.out.println("Enter radius");
+        double r = radius.nextDouble();
+        System.out.println("Radius is: " + 2*p*r);
+    }
+}
+class ph{
+    public static void phone(){
+        Scanner duration = new Scanner(System.in);
+        System.out.println("Enter duraion of first call --> ");
+        double t1 = duration.nextDouble();
+        System.out.println("Enter duraion of second call --> ");
+        double t2 = duration.nextDouble();
+        System.out.println("Enter duraion of third call --> ");
+        double t3 = duration.nextDouble();
+
+        System.out.println("Enter cost of minute in third call --> ");
+        double c1 = duration.nextDouble();
+        System.out.println("Enter cost of minute in third call --> ");
+        double c2 = duration.nextDouble();
+        System.out.println("Enter cost of minute in third call --> ");
+        double c3 = duration.nextDouble();
+        System.out.println("Cost of first call --> " + t1*c1);
+        System.out.println("Cost of second call --> " + t2*c2);
+        System.out.println("Cost of second call --> " + t3*c3);
+        double amount  =  t1*c1 + t2*c2 + t3*c3;
+        System.out.println("Cost of all calls --> " + amount);
+
+
+
+
+
+    }
+}
