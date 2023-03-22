1b2d6b3981c1f9bba19e439a8a236c39ab8450f9
[Fouzi3] <maksimgamesm2@gmail.com>
2023-03-22 19:38:21 +0200

    reworked

--- src/com/softserve/edu03/hw/hw_3.java
+++ src/com/softserve/edu03/hw/hw_3.java
@@ -6,20 +6,20 @@ public class hw_3 {
     public static void main(String[] args){
     triangle.area();
     }
-    class triangle{
-            public static void area(){
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Enter side a: ");
-                double a = sc.nextDouble();
-                System.out.println("Enter side b: ");
-                double b = sc.nextDouble();
-                System.out.println("Enter side c: ");
-                double c = sc.nextDouble();
+}
+class triangle{
+    public static void area(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter side a: ");
+        double a = sc.nextDouble();
+        System.out.println("Enter side b: ");
+        double b = sc.nextDouble();
+        System.out.println("Enter side c: ");
+        double c = sc.nextDouble();
 
-                double p = (a+b+c)/2;
+        double p = (a+b+c)/2;
 
-                double area = sqrt(p*(p-a)*(p-b)*(p-c));
-                System.out.println("The area of triangle = "+area);
-            }
+        double area = sqrt(p*(p-a)*(p-b)*(p-c));
+        System.out.println("The area of triangle = "+area);
     }
 }
---
+++ src/com/softserve/edu03/hw/hw_3_3.java
@@ -0,0 +1,7 @@
+public class hw_3_3 {
+    public static void main(String[] args){
+
+    }
+
+
+}
