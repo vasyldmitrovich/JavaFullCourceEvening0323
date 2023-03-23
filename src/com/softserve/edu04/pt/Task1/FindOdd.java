b156da7736f47964f10e5804a32698fc889d55af
Bozzhen <103925126+Bozzhen@users.noreply.github.com>
2023-03-24 01:21:10 +0200

    Committed Practical Task 1 from edu04

---
+++ src/com/softserve/edu04/pt/Task1/FindOdd.java
@@ -0,0 +1,23 @@
+package com.softserve.edu04.pt.Task1;
+
+public class FindOdd {
+    private int[] numbers;
+
+    public FindOdd(int... numbers) {
+        this.numbers = numbers;
+    }
+    public int[] getNumbers() {return numbers;}
+    public void setNumbers(int[] numbers) {this.numbers = numbers;}
+
+    public void odd() {
+        int counter = 0;
+        System.out.print("Odd numbers: " );
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                counter++;
+                System.out.print(number+"; ");
+            }
+        }
+        System.out.println("\nIt's " + counter + " out of " + numbers.length);
+    }
+}
---
+++ src/com/softserve/edu04/pt/Task1/Main.java
@@ -0,0 +1,9 @@
+package com.softserve.edu04.pt.Task1;
+
+public class Main {
+    public static void main(String [] args){
+        FindOdd findOdd = new FindOdd(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+        findOdd.odd();
+    }
+
+}
