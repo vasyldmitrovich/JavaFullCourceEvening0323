1a228247274a757d5251046f1f743af590c819f1
Danonchik-st <stavnykovych.b@nltu.lviv.ua>
2023-03-29 12:23:40 +0300

    Task

--- src/com/softserve/edu05/pt/Employee.java
+++ src/com/softserve/edu05/pt/Employee.java
@@ -1,8 +1,66 @@
 package com.softserve.edu05.pt;
 
+import java.util.Scanner;
+
 public class Employee {
   private String PIB;
   private int salary;
-  private  int DepartmentNumber;
+  private int DepartmentNumber;
 
-}
+  public Employee(String PIB, int salary, int DepartmentNumber) {
+    this.PIB = PIB;
+    this.salary = salary;
+    this.DepartmentNumber = DepartmentNumber;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Employee other = (Employee) obj;
+    if (DepartmentNumber != other.DepartmentNumber) return false;
+
+    return true;
+  }
+
+  public static void Cdepartment(int DepNubm1, int d) {
+    if (DepNubm1 == d) {
+      System.out.println();
+    }
+  }
+
+  @Override
+  public String toString() {
+
+    return "Employee{"
+            + "PIB='" + PIB
+            + ", salary=" + salary
+            + ", DepartmentNumber=" + DepartmentNumber
+            + '}';
+  }
+
+  public static void main(String[] args) {
+    Employee employer1 = new Employee("S.B.B", 30000, 1008);
+    Employee employer2 = new Employee("S.B.V", 50000, 1008);
+    Employee employer3 = new Employee("S.O.B", 20000, 1007);
+    int d;
+    Scanner br = new Scanner(System.in);
+    d = br.nextInt();
+
+    if(employer1.DepartmentNumber==d){
+      System.out.println( employer1.toString());
+    }
+    if(employer2.DepartmentNumber==d){
+      System.out.println( employer1.toString());
+    }
+    if(employer3.DepartmentNumber==d){
+      System.out.println( employer1.toString());
+    }
+
+
+
+
+
+  }
+  }
