315f597fec99baf02ab4daba89af34f624c4f815
Tanya Serbinenko <tanyaserbinenko1996@gmail.com>
2023-03-29 21:46:01 +0300

    commit

--- src/com/softserve/edu05/hw05/hw3/App.java
+++ src/com/softserve/edu05/hw05/hw3/App.java
@@ -2,15 +2,22 @@ package com.softserve.edu05.hw05.hw3;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
+import static com.softserve.edu05.hw05.hw3.CarService.findByYear;
 import static com.softserve.edu05.hw05.hw3.CarService.sortOfCars;
 
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         List cars = Arrays.asList(
                 new Car("Model1", 1995, 280),
                 new Car("Model2", 1997, 285),
                 new Car("Model3", 2000, 280),
                 new Car("Model4", 2005, 300));
         sortOfCars(cars);
+        System.out.println("Input year of car that you would like to find");
+        int year = sc.nextInt();
+        System.out.println(findByYear(year, cars));
     }
 }
--- src/com/softserve/edu05/hw05/hw3/CarService.java
+++ src/com/softserve/edu05/hw05/hw3/CarService.java
@@ -3,6 +3,7 @@ package com.softserve.edu05.hw05.hw3;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class CarService {
@@ -11,4 +12,8 @@ public class CarService {
         List sortedList = cars.stream().sorted(Comparator.comparingInt(Car::getYearOfProduction)).collect(Collectors.toList());
         sortedList.forEach(System.out::println);
     }
+
+    public static List <Car> findByYear (int year, List<Car> carList){
+        List sortedList = carList.stream().filter(m->m.getYearOfProduction()== year).collect(Collectors.toList());
+    return sortedList;}
 }
