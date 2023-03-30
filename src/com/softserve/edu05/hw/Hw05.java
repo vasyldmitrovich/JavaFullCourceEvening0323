552efa9276862b72270909e69a1c995a1ad41f69
groms1984 <groms1984@gmail.com>
2023-03-30 17:42:56 +0300

    RGonchar Pt05&Hw05

---
+++ src/com/softserve/edu05/hw/Hw05.java
@@ -0,0 +1,84 @@
+package src.com.softserve.edu05.hw;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hw05 {
+    public static void main(String[] args) {
+        //Task_1.1
+        System.out.println("Task 1.1");
+        System.out.print("Input month number(1..12): ");
+        Scanner sc = new Scanner(System.in);
+        int[] days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        int month = sc.nextInt();
+        System.out.println("This month has " + days[month] + " days.");
+
+        //Task_1.2
+        System.out.println("Task 1.2");
+        System.out.println("Please input 10 integer numbers: ");
+        int[] num = new int[10];
+        for(int i=0;i<=9;i++){
+            num[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(num));
+        if(num[0]>=0 && num[1]>=0 && num[2]>=0 && num[3]>=0 && num[4]>=0){
+            int sum=num[0]+num[1]+num[2]+num[3]+num[4];
+            System.out.println("Sum of first five elements is: " + sum);
+        }else{
+            int prod = num[5]*num[6]*num[7]*num[8]*num[9];
+            System.out.println("Product of last five elements is: " + prod);
+        }
+        //Task_1.3
+        System.out.println("Task 1.3");
+        System.out.println("Please input 5 integer numbers: ");
+        int[] five = new int[5];
+        for(int i=0;i<=4;i++){
+            five[i]=sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(five));
+        int counter=0;
+        for(int i=0;i<=4;i++){
+            if(five[i]>=0){
+                counter++;
+            }
+            if(counter==2){
+                System.out.println("Second positive element on position: " + i);
+                break;
+            }
+        }
+        int min = five[0];
+        int minind = 0;
+        int prod=1;
+        for(int ind=0; ind<=4; ind++){
+            if(five[ind]<min){
+                min=five[ind];
+                minind=ind;
+            }
+
+            if(five[ind]!=0 & five[ind]%2==0){
+                prod=prod*five[ind];
+            }
+        }
+        System.out.println("Min values is : " + five[minind] + " on position " + minind);
+        System.out.println("Product of elements is " + prod);
+
+
+        //Task_2
+        Scanner str = new Scanner(System.in);
+        System.out.println("Task 2");
+        boolean response = false;
+        do{
+            System.out.println("Please input 2 integer numbers: ");
+            int a =sc.nextInt();
+            int b =sc.nextInt();
+            System.out.println("a*b=" + a*b);
+            System.out.println("Would you like to continue? (yes/no)");
+            String resp = str.nextLine();
+            response = resp.equals("yes") ? true : false;
+        }while(response==true);
+    }
+
+}
+
+
---
+++ src/com/softserve/edu05/pt/Pt05.java
@@ -0,0 +1,61 @@
+package src.com.softserve.edu05.pt;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Pt05 {
+    public static void main(String[ ] args) {
+        //Task_1.1
+        String[] strArr = {"test","ab","bc","abc","test","sort"};
+        System.out.print("Source array: ");
+        for (String string : strArr) System.out.print(string + " ");
+        System.out.println();
+        System.out.print("Sorted array: ");
+        Arrays.sort(strArr);
+        for (String string : strArr) System.out.print(string + " ");
+        String elements = Arrays.toString(strArr);
+
+        //Task_1.2
+        System.out.println();
+        int [] avg = {1,2,3,4,6};
+        int sum=0;
+        for (int a : avg) sum=+a;
+        System.out.println("Average is: " + (double)sum/5.0);
+
+        //Task_1.3
+        Scanner sc = new Scanner(System.in);
+        String inpt = sc.nextLine();
+        int index = Arrays.binarySearch(strArr, inpt);
+        System.out.println("StrArray = " + elements);
+        System.out.println("Position of element with value " + inpt + " is: " + index);
+
+
+        Arrays.sort(avg);
+        String[] strOfint = Arrays.stream(avg)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+        int ind = Arrays.binarySearch(strOfint, inpt);
+        System.out.println("Int array = " + Arrays.toString(strOfint));
+        System.out.println("Position of element with value " + inpt + " is: " + ind);
+
+        //Task 2
+        System.out.print("Input an integer number to check if it's prime number: ");
+        int i,m=0,flag=0;
+        int n=sc.nextInt();
+        m=n/2;
+        if(n==0||n==1){
+            System.out.println(n+" is not prime number");
+        }else{
+            for(i=2;i<=m;i++){
+                if(n%i==0){
+                    System.out.println(n+" is not prime number");
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)  { System.out.println(n+" is prime number"); }
+        }
+
+
+    }
+}
