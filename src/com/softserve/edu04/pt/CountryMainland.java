ac6482fb37a20272a2239b196d648ac425027dee
NazarHome <torbak03@gmail.com>
2023-04-05 22:55:37 +0300

    country and days of week Pt complete

---
+++ src/com/softserve/edu04/pt/CountryMainland.java
@@ -0,0 +1,33 @@
+package com.softserve.edu04.pt;
+
+import java.util.Scanner;
+
+public class CountryMainland {
+    enum Continent {
+        AFRICA, ANTARCTICA, ASIA, AUSTRALIA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write a country name: ");
+        String country = scanner.nextLine().toUpperCase();
+
+        Continent continent;
+        switch (country) {
+            case "EGYPT", "KENYA", "SOUTH AFRICA" -> continent = Continent.AFRICA;
+            case "ANTARCTICA" -> continent = Continent.ANTARCTICA;
+            case "CHINA", "INDIA", "JAPAN" -> continent = Continent.ASIA;
+            case "AUSTRALIA", "NEW ZEALAND" -> continent = Continent.AUSTRALIA;
+            case "FRANCE", "GERMANY", "SPAIN" -> continent = Continent.EUROPE;
+            case "CANADA", "MEXICO", "UNITED STATES" -> continent = Continent.NORTH_AMERICA;
+            case "ARGENTINA", "BRAZIL", "COLOMBIA" -> continent = Continent.SOUTH_AMERICA;
+            default -> {
+                System.out.println("Country not found!");
+                return;
+            }
+        }
+
+        System.out.println("The name of the continent: " + continent);
+    }
+}
+
---
+++ src/com/softserve/edu04/pt/DaysOfWeek.java
@@ -0,0 +1,22 @@
+package com.softserve.edu04.pt;
+
+import java.util.Scanner;
+
+public class DaysOfWeek {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the number day of week?)");
+        int number = sc.nextInt();
+        var result = switch (number) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "eror404";
+        };
+        System.out.println(result);
+    }
+}
