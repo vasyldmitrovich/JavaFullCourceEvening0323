6eedd3c756d7ce1c42f348533890e8551da92858
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-04-10 11:39:06 +0300

    edu009_10

---
+++ src/com/softserve/edu09_10/wrapper/IEntity.java
@@ -0,0 +1,5 @@
+package com.softserve.edu09_10.wrapper;
+
+public interface IEntity {
+    int getId();
+}
---
+++ src/com/softserve/edu09_10/wrapper/UserEntity.java
@@ -0,0 +1,7 @@
+package com.softserve.edu09_10.wrapper;
+
+public class UserEntity implements IEntity {
+    public int getId() {
+        return 123;
+    }
+}
