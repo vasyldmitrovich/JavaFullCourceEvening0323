5a78ff7630c72da584793cdb9720ef7aeff08c62
sagittarii90 <sagittarii90@gmail.com>
2023-04-12 13:29:56 +0300

    Adding task #1 from HomeWork edu08

---
+++ src/com/softserve/edu08/hm08/Person.java
@@ -0,0 +1,32 @@
+package com.softserve.edu08.hm08;
+
+abstract class Person {
+    private FullName fullName;
+    private int age;
+
+    public Person(FullName fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public FullName getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(FullName fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    // Abstract method for displaying person information
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Abstract method for person's activity
+    public abstract void info();
+}
