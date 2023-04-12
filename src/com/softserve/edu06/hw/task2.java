e406126adc5a17265df5aa6509500c57b130cfdb
Danonchik-st <stavnykovych.b@nltu.lviv.ua>
2023-04-13 00:32:08 +0300

    task

---
+++ src/com/softserve/edu06/hw/task2.java
@@ -0,0 +1,22 @@
+package com.softserve.edu06.hw;
+
+
+public class task2 {
+    public static void main(String[] args) throws CloneNotSupportedException {
+         birds[] ArrayBirds = new birds[5];
+        ArrayBirds[0] = new Penguin("No","Yes");
+        ArrayBirds[1] = new Eagle("Yes"," Yes");
+        ArrayBirds[2] = new Kiwi("No","Yes");
+        ArrayBirds[3] =new Swallow("Yes","Yes");
+        ArrayBirds[4] =new NoFlyingBird("Yes","Yes");
+
+
+         for(int i=0; i<5; i++) {
+             ArrayBirds[i].Mykd();
+             ArrayBirds[i].print();
+             ArrayBirds[i].fly();
+
+         }
+         }
+
+    }
