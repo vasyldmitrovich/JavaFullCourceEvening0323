c5ca48484579831f809624c5f0f8907a5f700a16
Oleksandr Moroziuk <sagittarii90@gmail.com>
2023-04-22 10:26:32 +0300

    adding homework from edu 12, without the assignment with an asterisk.

---
+++ src/com/softserve/edu12/hm12/task1/DivisionCalculator.java
@@ -0,0 +1,30 @@
+package com.softserve.edu12.hm12.task1;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class DivisionCalculator {
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(in)) {
+            out.println("Enter the first number:");
+            double num1 = scanner.nextDouble();
+            out.println("Enter the first number:");
+            double num2 = scanner.nextDouble();
+            double result = div(num1, num2);
+            out.println("Result: " + result);
+        } catch (Exception e) {
+            out.println("Error: " + e.getMessage());
+        }
+    }
+
+
+    //method, which calculates the dividing of two double type numbers
+    public static double div(double num1, double num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return num1 / num2;
+    }
+}
---
+++ src/com/softserve/edu12/hm12/task2/NumberReader.java
@@ -0,0 +1,41 @@
+package com.softserve.edu12.hm12.task2;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class NumberReader {
+    /*that read from console integer number and return it, if it is in the range [start...end].
+    If an invalid number or non-number text is read, the method should throw an exception
+     */
+    public static int readNumber(int start, int end) throws Exception {
+        Scanner scanner = new Scanner(in);
+        out.print("Enter an integer between " + start + " and " + end + ": ");
+        if (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+            if (num >= start && num <= end) {
+                return num;
+            } else throw new Exception("Number is not in the valid range.");
+        } else {
+            throw new Exception("Invalid input. Please enter an integer.");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        int start = 1;
+        int end = 100;
+        try {
+            for (int i = 0; i < 10; i++) {
+                numbers[i] = readNumber(start, end);
+                start = numbers[i] + 1; // Ensure numbers are increasing
+            }
+            out.println("Successfully read 10 numbers:");
+            for (int i = 0; i < 10; i++) {
+                out.print(numbers[i] + " ");
+            }
+        } catch (Exception e) {
+            out.println("Error: " + e.getMessage());
+        }
+    }
+}
