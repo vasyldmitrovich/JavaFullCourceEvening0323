369aa18c3178b7152ff9bd0ae96ed5a3f8421c55
Ivan Yashchuk <ivanko3224@gmail.com>
2023-05-01 12:29:58 +0300

    add edu08 PT

---
+++ src/com/softserve/edu08/pt/AppP_8_1.java
@@ -0,0 +1,27 @@
+package com.softserve.edu08.pt;
+
+public class AppP_8_1 {
+    public static void main(String[] args) {
+        Department.Address address = new Department.Address("Ternopil", "Valova", 11);
+        Department department = new Department("Sales", address);
+
+        System.out.println("Department name: " + department.getName());
+        System.out.println("Department address: " + department.getAddress().getBuilding() + " " +
+                department.getAddress().getStreet() + ", " + department.getAddress().getCity());
+
+        Department department2 = null;
+        try {
+            department2 = department.clone();
+            department2.getAddress().setCity("Lviv");
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Clone Error!!");
+        }
+
+        System.out.println("Department 2:");
+        System.out.println("Name: " + department2.getName());
+        System.out.println("Address: " + department2.getAddress().getBuilding() + " " +
+                department2.getAddress().getStreet() + ", " + department2.getAddress().getCity());
+
+
+    }
+}
---
+++ src/com/softserve/edu08/pt/Department.java
@@ -0,0 +1,79 @@
+package com.softserve.edu08.pt;
+
+public class Department implements Cloneable {
+
+    private String name;
+    private Address address;
+
+    public Department(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public Department clone() throws CloneNotSupportedException {
+        Department cloned = (Department) super.clone();
+        cloned.address = this.address.clone();
+        return cloned;
+    }
+
+
+    public static class Address implements Cloneable {
+        private String city;
+        private String street;
+        private int building;
+
+        public Address(String city, String street, int building) {
+            this.city = city;
+            this.street = street;
+            this.building = building;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public int getBuilding() {
+            return building;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public void setBuilding(int building) {
+            this.building = building;
+        }
+
+        @Override
+        public Address clone() throws CloneNotSupportedException {
+            return (Address) super.clone();
+        }
+    }
+
+
+}
