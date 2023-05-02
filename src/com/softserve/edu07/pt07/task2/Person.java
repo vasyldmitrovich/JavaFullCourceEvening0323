fbe300ddb4cb3997799c3592662db57536c270b3
Oleksandr Moroziuk <sagittarii90@gmail.com>
2023-05-02 21:24:17 +0300

    made the field closed and added getters and setters; removed the words public abstract

--- src/com/softserve/edu07/pt07/task2/Person.java
+++ src/com/softserve/edu07/pt07/task2/Person.java
@@ -2,11 +2,20 @@ package com.softserve.edu07.pt07.task2;
 
 public abstract class Person {
     //Make field private and add getters and setters
-    String name;
+    private String name;
     public Person(String name) {
         this.name = name;
     }
 
-    //Del words public abstract, it will be public abstract by default
-    public abstract void print();
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    void print(){
+
+    }
 }
