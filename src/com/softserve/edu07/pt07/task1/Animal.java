79928fc9e7db2e17044c1035d4a547805f85241f
Oleksandr Moroziuk <sagittarii90@gmail.com>
2023-05-02 21:20:20 +0300

    methods changed the access level from "default" to "abstract", and unnecessary brackets {} were removed

--- src/com/softserve/edu07/pt07/task1/Animal.java
+++ src/com/softserve/edu07/pt07/task1/Animal.java
@@ -1,11 +1,7 @@
 package com.softserve.edu07.pt07.task1;
 
 public interface Animal {
-
-    //This method must be abstract, please delete word default and {}
-    default void voice(){};
-    default void feed(){};
-
-
+    abstract void voice();
+    abstract void feed();
 
 }
