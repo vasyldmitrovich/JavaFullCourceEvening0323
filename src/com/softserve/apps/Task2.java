2fd9803d3e184497c541e8fcf2cdb5a74b7e0879
Oleksandr Moroziuk <sagittarii90@gmail.com>
2023-05-08 19:59:09 +0300

    add task2

---
+++ src/com/softserve/apps/Task2.java
@@ -0,0 +1,30 @@
+package com.softserve.apps;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        print(number);
+    }
+
+    public static void print(int number) {
+        System.out.print("Prime factorization of " + number + ": ");
+        for (int i = 2; i <= number; i++) {
+            int count = 0;
+            while (number % i == 0) {
+                count++;
+                number /= i;
+            }
+            if (count > 0) {
+                if (count == 1) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + "^" + count + " ");
+                }
+            }
+        }
+    }
+}
