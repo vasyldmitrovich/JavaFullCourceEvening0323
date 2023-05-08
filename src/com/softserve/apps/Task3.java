ae40dc60b3490f34d815b46b14b62a32a34ccedb
Ivan Yashchuk <ivanko3224@gmail.com>
2023-05-08 19:46:52 +0300

    add task3

---
+++ src/com/softserve/apps/Task3.java
@@ -0,0 +1,27 @@
+package com.softserve.apps;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter dollar cost --> ");
+        double DolarCost = sc.nextDouble();
+        System.out.println("Enter how mutch gryvnas you have --> ");
+        double GryvnaAmount = sc.nextDouble();
+
+        System.out.println("You can buy  " + BuyDollar(DolarCost,GryvnaAmount) + "$");
+        System.out.println("You will have "+ ChangeDollar(DolarCost)+" gryvnas");
+    }
+
+    public static double BuyDollar(double DollarCost, double GryvnaAmount){
+        double dollars = GryvnaAmount / DollarCost;
+        return dollars;
+    }
+
+    public static double ChangeDollar(double DollarCost){
+        double gryvnas = DollarCost * DollarCost;
+        return gryvnas;
+    }
+}
+
